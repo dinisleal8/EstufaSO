d2acd4174d1d841bf17eb21a50e8f860bb48babb
Kento193 <120380590+Kento193@users.noreply.github.com>
2022-12-12 11:58:50 +0000

    Update base.c

--- base.c
+++ base.c
@@ -13,3 +13,16 @@ list lista[3] {"letuce","carrot", "onion" };
 lista.hum_a[1] = 0.7; // 70%
 lista.temp[1] = 25; // 25ºc
 lista.hum_a[1] = 
+
+  
+//Dados Cenoura
+  lista.temp[2] = 
+  lista.hum_s[2] = 
+  lista.hum_a[2] = 
+  lista.light[2] = 
+  
+  //Dados Cebola
+  lista.temp[3]
+  lista.hum_s[3]
+  lista.hum_a[3]
+  lista.light[3]
