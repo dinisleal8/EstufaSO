1e4a7399b188d2f27cf9d0c77d05ece014c47072
dinisleal8 <81867403+dinisleal8@users.noreply.github.com>
2022-12-12 11:56:05 +0000

    Update base.c

--- base.c
+++ base.c
@@ -1,3 +1,15 @@
 
 
-struct list[3] {"letuce","carrot", " }
+struct list{
+  int temp;
+  int hum_s;
+  int hum_a;
+  int light;
+};
+
+list lista[3] {"letuce","carrot", "onion" };
+
+//Dados alface
+lista.hum_a[1] = 0.7; // 70%
+lista.temp[1] = 25; // 25ºc
+lista.hum_a[1] = 
